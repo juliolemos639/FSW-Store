da1b2f16f4cf17f089a18b9676f54e81a97046a0
juliolemos639 <julio.lemos639@gmail.com>
2023-10-18 18:27:03 -0300

    feat: add SectionTitle and PromoBanner components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
+// Notion
+
+// https://narrow-beach-a00.notion.site/Primeira-Aula-9d64b6a7fbe84ebb90ebc6498a00000f
+
 import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
-
-// Notion
-
-// https://narrow-beach-a00.notion.site/Primeira-Aula-9d64b6a7fbe84ebb90ebc6498a00000f
+import SectionTitle from "./components/section-title";
+import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -16,32 +18,59 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
-    <div>
-      <Image
-        src="/Banner-home-01.png"
-        alt="Até 55% de desconto esse mês"
-        height={0}
-        width={0}
-        className="h-auto w-full"
-        sizes="100vw"
+    <div className="flex flex-col gap-8 py-8">
+      <PromoBanner
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto esse mês!"
       />
-      <div className="mt-8 px-5">
+
+      <div className="px-5">
         <Categories />
       </div>
-      <div className="mt-8">
-        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
+
+      <div>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
 
-      <Image
-        src="/Banner-home-02.png"
-        alt="Até 55% de desconto em mouses"
-        height={0}
-        width={0}
-        className="h-auto w-full"
-        sizes="100vw"
+      <PromoBanner
+        src="/banner-home-02.png"
+        alt="Até 55% de desconto em mouses!"
       />
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+      </div>
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
+      </div>
     </div>
   );
 }

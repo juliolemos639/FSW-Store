47cb82d1db90e29e4c3047ecda9cfbcc43f7d2d4
juliolemos639 <julio.lemos639@gmail.com>
2023-10-18 07:15:46 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+
+// Notion
+
+// https://narrow-beach-a00.notion.site/Primeira-Aula-9d64b6a7fbe84ebb90ebc6498a00000f
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/Banner-home-01.png"
+        alt="Até 55% de desconto esse mês"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
+}

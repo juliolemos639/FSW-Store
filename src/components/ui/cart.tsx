6758a8ca1b8d351ad6158d59a44192c891a13ff4
juliolemos639 <julio.lemos639@gmail.com>
2023-10-20 20:27:24 -0300

    feat: add stripe checkout to cart products

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,21 +7,23 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
-// import { createCheckout } from "@/actions/checkout";
-// import { loadStripe } from "@stripe/stripe-js";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
+
+// npm install @stripe/stripe-js
 
 const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
-  // const handleFinishPurchaseClick = async () => {
-  //   const checkout = await createCheckout(products);
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
 
-  //   const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
-  //   stripe?.redirectToCheckout({
-  //     sessionId: checkout.id,
-  //   });
-  // };
+    stripe?.redirectToCheckout({
+      sessionId: checkout.id,
+    });
+  };
 
   return (
     <div className="flex h-full flex-col gap-8">
@@ -85,7 +87,7 @@ const Cart = () => {
 
           <Button
             className="mt-7 font-bold uppercase"
-            // onClick={handleFinishPurchaseClick}
+            onClick={handleFinishPurchaseClick}
           >
             Finalizar compra
           </Button>

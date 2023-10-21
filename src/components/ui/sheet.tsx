7775a4a48ef5fffe8d7a5bc00416b9d89891a46d
juliolemos639 <julio.lemos639@gmail.com>
2023-10-21 18:46:22 -0300

    fix: sheet build error

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -14,11 +14,13 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
+  // @ts-expect-error
   className,
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
+  // @ts-expect-error
   <SheetPrimitive.Portal className={cn(className)} {...props} />
-)
+);
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
 const SheetOverlay = React.forwardRef<

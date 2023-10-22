9ee53ec8b9f3d6e35aedf0274f470afb01ceb47a
juliolemos639 <julio.lemos639@gmail.com>
2023-10-21 22:56:53 -0300

    fix: back images  to public path

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,6 @@ import { prismaClient } from "@/lib/prisma";
 import ProductList from "../../components/ui/product-list";
 import SectionTitle from "../../components/ui/section-title";
 import PromoBanner from "./components/promo-banner";
-import banner01 from "../../../../fsw-store/public/Banner-home-01.png";
-import banner02 from "../../../../fsw-store/public/Banner-home-02.png";
-import banner03 from "../../../../fsw-store/public/Banner-home-03.png";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -35,8 +32,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-8 py-8">
       <PromoBanner
-        src={banner01}
-        // src="/banner-home-01.png"
+        src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês!"
       />
 
@@ -50,8 +46,7 @@ export default async function Home() {
       </div>
 
       <PromoBanner
-        src={banner02}
-        // src="/banner-home-02.png"
+        src="/banner-home-02.png"
         alt="Até 55% de desconto em mouses!"
       />
 
@@ -62,8 +57,7 @@ export default async function Home() {
 
       <div>
         <PromoBanner
-          src={banner03}
-          // src="/banner-home-03.png"
+          src="/banner-home-03.png"
           alt="Até 55% de desconto em mouses!"
         />
       </div>
